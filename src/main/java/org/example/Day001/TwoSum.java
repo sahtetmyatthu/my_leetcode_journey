58c22bb11c88e5f1092caae6a5e4bcf78cd78a91
sahtetmyatthu <mrngasa96@gmail.com>
2025-03-18 00:04:32 +0630

    day1 of the leetcode journey

---
+++ src/main/java/org/example/Day001/TwoSum.java
@@ -0,0 +1,31 @@
+package org.example.Day001;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] num, int target){
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < num.length; i++){
+            int complement = target - num[i];// What number do we need?
+
+            // If we already saw the complement, return the answer
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+            // Store the current number and its index
+            map.put(num[i], i);
+        }
+
+        throw  new IllegalArgumentException("No solution found");
+    }
+
+    public static void main(String[] args) {
+        int[] num = {2,7,11,15};
+        int target = 9;
+
+        int[] result = twoSum(num, target);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
