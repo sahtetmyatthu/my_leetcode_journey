a65213b1128c0d2d9b983ef2eb27bf325bfd5ca2
ngasa <ngasa@192.168.1.99>
2025-05-30 09:57:20 +0630

    Day 8 of my leetcode journey

---
+++ src/main/java/org/example/Day008/ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+package org.example.Day008;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.min(height[left], height[right]);
+            int currentArea = width * minHeight;
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+
+    }
+}
