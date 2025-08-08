6e3461aec409799059338aabfbdbb3dc97435ddf
ngasa <mrngasa96@gmail.com>
2025-08-08 09:53:01 +0630

    still try to understand the continuous array problem :(

--- src/main/java/org/example/Day013/EqualBinarySubarrayFast.java
+++ src/main/java/org/example/Day013/EqualBinarySubarrayFast.java
@@ -1,48 +1,26 @@
 package org.example.Day013;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class EqualBinarySubarrayFast {
     public int findMaxLength(int[] nums) {
-        // Step 1: Convert 0 to -1
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                nums[i] = -1;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for(int i=0 ; i < nums.length; i ++){
+            count = count + (nums[i] == 1? 1: -1);
+            if (map.containsKey(count)){
+                maxlen = Math.max(maxlen, i - map.get(count));
+            }else {
+                map.put(count, i);
             }
         }
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);  // Base case: sum 0 before index 0
+        return maxlen;
 
-        int maxLength = 0;
-        int sum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            System.out.println("Index: " + i + ", Current number: " + nums[i] + ", Running sum: " + sum);
-
-            if (map.containsKey(sum)) {
-                int prevIndex = map.get(sum);
-                int currentLength = i - prevIndex;
-                System.out.println("  Sum " + sum + " seen before at index " + prevIndex + ". Subarray length: " + currentLength);
-
-                if (currentLength > maxLength) {
-                    maxLength = currentLength;
-                    System.out.println("  New maxLength found: " + maxLength);
-                }
-            } else {
-                map.put(sum, i);
-                System.out.println("  First time sum " + sum + " encountered at index " + i);
-            }
-        }
-
-        System.out.println("Final maxLength: " + maxLength);
-        return maxLength;
     }
 
-    public static void main(String[] args) {
-        EqualBinarySubarrayFast solution = new EqualBinarySubarrayFast();
-        int[] nums = {0, 1, 1, 0, 0};
-        solution.findMaxLength(nums);
-    }
+
 }
