c83e77c4253be97520325a048cfc75ae9fed1506
ngasa <mrngasa96@gmail.com>
2025-08-15 09:47:25 +0630

    i think a little bit understand :)

--- src/main/java/org/example/Day013/EqualBinarySubarrayFast.java
+++ src/main/java/org/example/Day013/EqualBinarySubarrayFast.java
@@ -1,26 +1,42 @@
 package org.example.Day013;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class EqualBinarySubarrayFast {
     public int findMaxLength(int[] nums) {
 
-        Map<Integer, Integer> map = new HashMap<>();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
-        int maxlen = 0, count = 0;
-        for(int i=0 ; i < nums.length; i ++){
-            count = count + (nums[i] == 1? 1: -1);
-            if (map.containsKey(count)){
-                maxlen = Math.max(maxlen, i - map.get(count));
-            }else {
-                map.put(count, i);
+
+        int maxLength = 0;
+        int sum = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            sum += (nums[i] == 1 )? 1: -1;
+
+            if(map.containsKey(sum)) {
+                int prevIndex = map.get(sum);
+                int currentLength = i - prevIndex;
+
+                if(currentLength > maxLength) {
+                    maxLength = currentLength;
+                }
+            } else {
+                map.put(sum, i);
             }
+
         }
 
-        return maxlen;
+        return maxLength;
+
 
     }
 
-
+    public static void main(String[] args) {
+        EqualBinarySubarrayFast solver = new EqualBinarySubarrayFast();
+        int[] nums = {0, 1, 0, 1, 1, 0, 0};
+        int result = solver.findMaxLength(nums);
+        System.out.println("\nFinal maxLength: " + result);
+    }
 }
