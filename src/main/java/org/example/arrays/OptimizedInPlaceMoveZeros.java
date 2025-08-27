f73f747bbce3dde09be3ccaf8527d2e4022df775
ngasa <mrngasa96@gmail.com>
2025-08-27 10:10:26 +0630

    Leetcode 169: Majority Element

---
+++ src/main/java/org/example/arrays/OptimizedInPlaceMoveZeros.java
@@ -0,0 +1,34 @@
+package org.example.arrays;
+
+public class OptimizedInPlaceMoveZeros {
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 0, 3, 12};
+
+        System.out.println("Before moveZeroes:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+        moveZeroes(nums);
+
+        System.out.println("\nAfter moveZeroes:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int lastZeroPositions = 0;
+
+        for(int current = 0; current < nums.length; current++){
+            if(nums[current] != 0){
+                int temp = nums[lastZeroPositions];
+                nums[lastZeroPositions] = nums[current];
+                nums[current] = temp;
+                lastZeroPositions++;
+
+            }
+        }
+    }
+}
